1eb3bd9e7f4e6edeed3794ade4cd02851fcf57b0
ambuilding <explorewo@gmail.com>
2016-06-25 16:43:06 +0800

    Add files via upload

---
+++ CassiniViewController.swift
@@ -0,0 +1,114 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by WangQi on 16/5/25.
+//  Copyright © 2016年 WangQi. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController, UISplitViewControllerDelegate {
+    
+    /*
+     this is just our normal "put constants in a struct" thing 
+     but we call it Storyboard, because all the constants in it
+     are strings in our Storyboard
+     */
+    private struct Storyboard {
+        static let ShowImageSegue = "Show Image"
+    }
+    
+    // prepare for segue is called
+    // even if we invoke the segue from code using perforSegue 
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == Storyboard.ShowImageSegue {
+            if let ivc = segue.destinationViewController.contentViewController as? ImageViewController {
+                //if let sendingButton = sender as? UIButton  {
+                //}
+                let imageName = (sender as? UIButton)?.currentTitle
+                ivc.imageURL = DemoURL.NASAImageNamed(imageName)
+                ivc.title = imageName
+            }
+        }
+    }
+    
+    /*
+     we changed the buttons to this target/action method 
+     so that we could either
+     a. just set our imageURL in teh detail if we're in a split view, or 
+     b. cause the segue to happen from code with performSegue
+     to make teh latter work, we had to create a segue in our storyboard
+     that was ctrl-dragged from the view controller icon (orange one at the top)
+     */
+    
+    @IBAction func showImage(sender: UIButton) {
+        
+        if let ivc = splitViewController?.viewControllers.last?.contentViewController as? ImageViewController {
+            let imageName = sender.currentTitle
+            ivc.imageURL = DemoURL.NASAImageNamed(imageName)
+            ivc.title = imageName
+
+        } else {
+            performSegueWithIdentifier(Storyboard.ShowImageSegue, sender: sender)
+        }
+        
+    }
+    
+    /*
+     if we are in a split view, we set ourselves as its delegate
+     this is so we can prevent an empty detail from collapsing on top of our master
+     see split view delegate method below
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        splitViewController?.delegate = self
+    }
+    
+    /*
+     this method lets the split view's delegate
+     collapse the detail on top of the master when it's the detail's time to appear 
+     this method returns whether we (the delegate) handled doing this 
+     we don't want an empty detail to collapse on top of our master
+     so if the detail is an empty ImageViewController, we return true 
+     (which tells the split view controller that we handled the collapse)
+     of course, we didn't actually handle it, we did nothing 
+     but that's exactly what we want (i.e. no collapse if the detail ivc is empty)
+     */
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
+        
+        if primaryViewController.contentViewController == self {
+            if let ivc = secondaryViewController.contentViewController as? ImageViewController
+                where ivc.imageURL == nil {
+                // 没有点击button，也就没有 button title，也就没有为imageURL赋值，为nil
+                return true
+            }
+        }
+        return false
+    }
+}
+
+/*
+ a little helper extension 
+ which eithe returns the view controller you send it to 
+ or, if you send it to a UINavigationController,
+ it returns its visibleViewController
+ (if any, otherwise the UINavigationController itself)
+ */
+
+extension UIViewController {
+    
+    var contentViewController: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+    
+}
+// extension 为了在iPad里ImageViewController 用到 UINavigationController，而iPhone 不需要。
---
+++ ImageViewController.swift
@@ -0,0 +1,149 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by WangQi on 16/5/25.
+//  Copyright © 2016年 WangQi. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+    
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            /*
+             we'll postpone our (expensive) fetch
+             until we know we're going to appear on screen
+             (this below in the viewWillApear)
+             otherwise why waste the resources?
+ */
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    func fetchImage() {
+        if let url = imageURL {
+            /*
+             fire up the spinner
+             because we're about to fork something off on another thread
+             */
+            spinner?.startAnimating()
+            /*
+             put a closure on the "user initiated" system queue
+             this closure does NSData(contentsOfURL:) which blocks
+             waiting for network response
+             it's fine for it to block the "user initiated" queue
+             because that's a concurrent queue
+             (so other closures on that queue can run concurrently even as this one's blocked)
+             */
+            
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) {
+                let contentsOfURL = NSData(contentsOfURL: url) // blocks! can't be on main queue!
+                
+                /* 
+                 now that we got the data from the network
+                 we want to put it up in the UI
+                 but we can only do that on the main queue
+                 so we queue up a closure here to do that
+                 */
+                dispatch_async(dispatch_get_main_queue()) {
+                    /*
+                     since it could take a long time to fetch the image data
+                     we make sure here that the image we fetched 
+                     is still the one this ImageViewController wants to display!
+                     we must always think of these sorts of things
+                     when programming asynchronously异步的
+                     */
+                    if url == self.imageURL {
+                        if let imageData = contentsOfURL {
+                            self.image = UIImage(data: imageData)
+                            // image's set will stop the spinner animating
+                        } else {
+                            self.spinner?.stopAnimating()
+                        }
+                    } else {
+                        // just so we can see in the console when this happens
+                        print("Ignored data returned from url \(url)")
+                    }
+                }
+            } // 取图片的同时，可以进行其它操作，比如返回到Navcon
+        }
+    }
+    // 取图片，通过url，得到图片等数据，UIImage，解析数据
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+            /*
+             all three of the next lines of code 
+             are necessary to make zooming work
+             */
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+        }
+    }
+    
+    // zooming will not work if we don't implement
+    // this UIScrollViewDelegate method
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    private var imageView = UIImageView()
+    
+    /*
+     a little helper var
+     it just makes sure things are kept in sync
+     whenever we change the image we're displaying 
+     it's purely to make our code look prettier elsewhere in this class
+     */
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating() // 取到图片以后停止spinner
+        }
+    }
+    
+    // MARK: View Controller Lifecycle
+    
+    /*
+     we know we're going to go on screen in this method
+     so we can no longer wait to fire off our (expensive) image fetch
+     */
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+
+    /*
+     note that we build some of our UI in the storyboard
+     by dragging a UIScrollView out into our scene
+     and we build some of it here by adding our UIImageView
+     as a subview of the UIScrollView
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+        //imageURL = NSURL(string: DemoURL.Stanford)
+    }
+    
+}
